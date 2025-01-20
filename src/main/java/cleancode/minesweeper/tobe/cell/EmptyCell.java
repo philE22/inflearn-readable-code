e42db418858eece15567f5015366edd19b76ea26
philE22 <moonwhlking@gmail.com>
2025-01-20 23:52:09 +0900

    Step 12. LSP 부모 클래스를 자식 클래스로 교체해도 정상 동작해야한다 - 각 종류의 Cell 을 자식클래스로 변경

---
+++ src/main/java/cleancode/minesweeper/tobe/cell/EmptyCell.java
@@ -0,0 +1,29 @@
+package cleancode.minesweeper.tobe.cell;
+
+public class EmptyCell extends Cell {
+
+    private static final String EMPTY_CELL_SIGN = "■";
+
+    @Override
+    public boolean isLandMine() {
+        return false;
+    }
+
+    @Override
+    public boolean hasNearbyLandMineCount() {
+        return false;
+    }
+
+    @Override
+    public String getSign() {
+        if (isOpened) {
+            return EMPTY_CELL_SIGN;
+        }
+
+        if (isFlagged) {
+            return FLAG_SIGN;
+        }
+
+        return UNCHECKED_CELL_SIGN;
+    }
+}
