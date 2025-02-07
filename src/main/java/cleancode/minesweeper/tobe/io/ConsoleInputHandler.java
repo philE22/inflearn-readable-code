5e98d6a4507f6fe696a488b30f6436aaa728a733
philE22 <moonwhlking@gmail.com>
2025-02-08 02:25:39 +0900

    Step 17. 일급 컬렉션

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
@@ -20,9 +20,9 @@ public class ConsoleInputHandler implements InputHandler{
     public CellPosition getCellPositionFromUser() {
         String userInput = SCANNER.nextLine();
 
-        int colIndex = boardIndexConverter.getSelectedColIndex(userInput);
         int rowIndex = boardIndexConverter.getSelectedRowIndex(userInput);
+        int colIndex = boardIndexConverter.getSelectedColIndex(userInput);
 
-        return CellPosition.of(colIndex, rowIndex);
+        return CellPosition.of(rowIndex, colIndex);
     }
 }
