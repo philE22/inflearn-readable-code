47c56e48cc802b4d795bb63227456a1386bf9f6c
philE22 <moonwhlking@gmail.com>
2025-02-16 16:55:48 +0900

    Step 24. 버그수정 - 객체지향적 설계로 쉽게 문제가 되는 부분을 수정할 수 있었음

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
@@ -22,10 +22,6 @@ public class CellState {
         isOpened = true;
     }
 
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
     public boolean isOpened() {
         return isOpened;
     }
